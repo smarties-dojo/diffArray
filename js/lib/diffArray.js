813ed9c27d44c867eeb1dcdbabe2a3f1046a666d
Knaebel <Roy.Knaebel@trumpf.com>
2017-12-20 21:22:32 +0100

    add implementation

--- js/lib/diffArray.js
+++ js/lib/diffArray.js
@@ -2,8 +2,16 @@
 
 const differ = {};
 
-differ.GetExclusiveElementsOfFirstArray = (a1, a2) => {
-    return [];
-};
+differ.diffArray = (a1, a2) => {
+    return getExclusiveElementsOfFirstArray(a1, a2).concat(getExclusiveElementsOfFirstArray(a2, a1));
+}
+
+var getExclusiveElementsOfFirstArray = (a1, a2) => {
+    return a1.filter(x => {
+        return !a2.some(y => {
+            return x == y;
+        })
+    });
+} 
 
 module.exports = differ;
